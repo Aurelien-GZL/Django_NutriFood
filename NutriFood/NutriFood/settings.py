305b45c8f097a0904497bc63cafee61b7782df1b
Aurelien_GZL <113213543+Aurelien-GZL@users.noreply.github.com>
2023-07-07 16:44:26 +0200

    Django WCS Quest 3 (database)

--- NutriFood/NutriFood/settings.py
+++ NutriFood/NutriFood/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'nutrifood',
         'USER': 'aurelien',
-        'PASSWORD': 'aurelien',
+        #'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
