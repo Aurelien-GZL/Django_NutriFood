ed72e69ba55b6c6795d0cf544ef2705579accee3
Aurelien <aurelien.gazengel@gmail.com>
2023-07-11 15:08:28 +0200

    Django Quest 6 WCS (dynamic views)

--- NutriFood/search/urls.py
+++ NutriFood/search/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Path: /search/
     path('', views.index, name='index'),
+    # Path: /search/<product_id>/
+    # Referring to view "product" in /search/views.py
+    path('<str:product_id>/', views.product_detail, name='product_detail'),
 ]
--- NutriFood/search/views.py
+++ NutriFood/search/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Product
 from django.http import HttpResponse
 
+# Create your views here.
 
 def index(request):
-    return HttpResponse("Hey tu es sur l'index de l'application search du projet NutriFood.")
+    product_all = Product.objects.all()
+    context = {"product_all": product_all}
+    return render(request, "search/index.html", context)
+
+# Product_id is extracted from the URL matching the "product_detail" pattern in search/urls.py
+def product_detail(request, product_id):
+    # Access "Product" object database and get records from the URL product_id
+    product = Product.objects.get(id=product_id)
+    # Dictionary holding data to pass to the template, here product records for the given product_id
+    context = {"product": product}
+    # Render function takes the request, the template path, and the context dictionary as arguments
+    # This function combines the template with the provided context data and generates an HTML response, which is sent back to the client.
+    return render(request, "search/product_detail.html", context)
