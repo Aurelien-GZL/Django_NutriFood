b97b05b921f20ffb6624e6fb2ae12154a103c4eb
Aurelien <aurelien.gazengel@gmail.com>
2023-07-12 18:27:21 +0200

    Django Quest 7 WCS (forms)

--- NutriFood/search/urls.py
+++ NutriFood/search/urls.py
@@ -2,10 +2,19 @@ from django.urls import path
 
 from . import views
 
+# Create namespace for the application
+app_name = "search"
+
 urlpatterns = [
     # Path: /search/
     path('', views.index, name='index'),
+    # Path: /search/compare/
+    # Referring to view "product_compare" in /search/views.py
+    # Be careful to the order of the paths to avoid wrong referencing due to the more generic pattern
+    # Here product_compare won't work if below product detail
+    path('compare/', views.product_compare, name='product_compare'),
     # Path: /search/<product_id>/
-    # Referring to view "product" in /search/views.py
+    # Referring to view "product_detail" in /search/views.py
     path('<str:product_id>/', views.product_detail, name='product_detail'),
+
 ]
--- NutriFood/search/views.py
+++ NutriFood/search/views.py
@@ -9,6 +9,7 @@ def index(request):
     context = {"product_all": product_all}
     return render(request, "search/index.html", context)
 
+
 # Product_id is extracted from the URL matching the "product_detail" pattern in search/urls.py
 def product_detail(request, product_id):
     # Access "Product" object database and get records from the URL product_id
@@ -18,3 +19,39 @@ def product_detail(request, product_id):
     # Render function takes the request, the template path, and the context dictionary as arguments
     # This function combines the template with the provided context data and generates an HTML response, which is sent back to the client.
     return render(request, "search/product_detail.html", context)
+
+
+def product_compare(request):
+    distinct_categories = Product.objects.values('category')\
+        .distinct()\
+        .order_by('category')
+    if request.method == 'POST':
+        selected_category = request.POST.get('category')
+        selected_product = request.POST.get('product_name')
+        related_products = Product.objects.filter(category=selected_category)\
+            .order_by('product_name')
+        alternative_products = Product.objects.filter(category=selected_category)\
+            .order_by('nutriscore_score')[:5]
+        try:
+            product_info = Product.objects.get(product_name=selected_product)
+        except Product.DoesNotExist:
+            product_info = None
+        context = {
+            "product_info": product_info,
+            "related_products": related_products,
+            "distinct_categories": distinct_categories,
+            "selected_category": selected_category,
+            "alternative_products": alternative_products,
+            "selected_product": selected_product
+        }
+        return render(request, "search/product_compare.html", context)
+    else:
+        selected_category = request.GET.get('category')  # Get the selected category from the query parameters
+        related_products = Product.objects.filter(category=selected_category) \
+            .order_by('product_name')
+        context = {
+            "related_products": related_products,
+            "distinct_categories": distinct_categories,
+            "selected_category": selected_category,
+    }
+        return render(request, "search/product_compare.html", context)
